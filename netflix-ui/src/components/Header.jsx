a0c221052f5a8cb6caf1394518bde5413f7db3c5
salimbh <sloomabenhalima95@gmail.com>
2023-02-12 23:55:29 +0900

    set up files

---
+++ netflix-ui/src/components/Header.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+import logo from "../assets/logo.png";
+
+const Header = () => {
+  return (
+    <StyledHeader className="flex a-center j-between">
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <button onClick={() => navigate(props.login ? "/login" : "/signup")}>
+        {props.login ? "Log In" : "Sign In"}
+      </button>
+    </StyledHeader>
+  );
+};
+
+const StyledHeader = styled.header`
+  padding: 0 4rem;
+  .logo {
+    img {
+      height: 5rem;
+    }
+  }
+  button {
+    padding: 0.5rem 1rem;
+    background-color: #e50914;
+    border: none;
+    cursor: pointer;
+    color: white;
+    border-radius: 0.2rem;
+    font-weight: bolder;
+    font-size: 1.05rem;
+  }
+`;
+export default Header;
---
+++ netflix-ui/src/pages/UserListedMovies.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserListedMovies = () => {
+  return <div>UserListedMovies</div>;
+};
+
+export default UserListedMovies;
