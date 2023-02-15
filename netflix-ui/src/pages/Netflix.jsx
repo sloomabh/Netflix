826b22f4e0444869dda76be84f053dd539ddd8e8
salimbh <sloomabenhalima95@gmail.com>
2023-02-15 12:13:22 +0900

    start navbar and netflix home page

--- netflix-ui/src/pages/Netflix.jsx
+++ netflix-ui/src/pages/Netflix.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import backgroundImage from "../assets/home.jpg";
+import MovieLogo from "../assets/homeTitle.webp";
 
 const Netflix = () => {
-  return <div>Netflix</div>;
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+
+  return (
+    <div>
+      <Navbar isScrolled={isScrolled} />
+    </div>
+  );
 };
 
 export default Netflix;
