afcdac022eff81ab364cca42c2895fa6f3aa5aad
salimbh <sloomabenhalima95@gmail.com>
2023-02-22 12:31:29 +0900

    change the database to the cloud

--- netflix-api/server.js
+++ netflix-api/server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const userRoutes = require("./routes/UserRoutes");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const app = express();
-
+dotenv.config({ path: "./config.env" });
 app.use(cors());
 app.use(express.json());
 
+/*
 mongoose
   .connect("mongodb://localhost:27017/netflix", {
     useNewUrlParser: true,
@@ -16,6 +18,20 @@ mongoose
   .then(() => {
     console.log("DB Connetion Successfull");
   })
+  .catch((err) => {
+    console.log(err.message);
+  });*/
+
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    //useCreateIndex: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false,
+  })
+  .then(() => console.log("DB connection successful!"))
   .catch((err) => {
     console.log(err.message);
   });
